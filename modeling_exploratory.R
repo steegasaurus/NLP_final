f93ed961b167e84bb48505664b3a7c34aa024363
Steeg Pierce <64050681+steegasaurus@users.noreply.github.com>
2020-12-21 16:28:05 -0600

    Update modeling_exploratory.R

--- modeling_exploratory.R
+++ modeling_exploratory.R
@@ -8,4 +8,6 @@ blogs <- read_lines('final/en_US/en_US.blogs.txt')
 news <- read_lines('final/en_US/en_US.news.txt')
 tweets <- readLines('final/en_US/en_US.twitter.txt')
 
-
+#I need to turn each sentence into a separate 'document'
+#I need to combine all 3 datasets
+#I need to tokenize the whole thing and remove unnecessary characters
