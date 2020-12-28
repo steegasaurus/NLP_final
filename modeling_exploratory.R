7f13040a1cfa0e2b2f4bfa618ccaa974ab27f278
Steeg Pierce <64050681+steegasaurus@users.noreply.github.com>
2020-12-27 21:39:27 -0600

    Update modeling_exploratory.R

--- modeling_exploratory.R
+++ modeling_exploratory.R
@@ -2,15 +2,52 @@ library(dplyr)
 library(readr)
 library(quanteda)
 library(tidyr)
-library(tidytext)
 
 blogs <- read_lines('final/en_US/en_US.blogs.txt')
 news <- read_lines('final/en_US/en_US.news.txt')
 tweets <- readLines('final/en_US/en_US.twitter.txt')
-combined <- corpus(c(blogs, news, tweets))
-sentences <- unlist(tokens(combined, what = 'sentence'))
-words <- tokens(sentences, 
+combinedRaw <- c(blogs, news, tweets)
+set.seed(42)
+n <- 10
+combined <- sample(combinedRaw, size = length(combinedRaw)/n)
+trigrams <- tokens(combined, 
                 remove_punct = T, 
                 remove_symbols = T, 
-                remove_numbers = T)
+                remove_numbers = T,
+                remove_separators = T,
+                remove_url = T) %>%
+    tokens_tolower %>%
+    tokens_ngrams(n = 3) %>%
+    unlist
+
+dfModel <- tibble(text = trigrams) %>%
+    separate(text, c('a', 'b', 'c'), sep = '_')
+
+findWord <- function(word1, word2){
+    if(word1 %in% dfModel$a & word2 %in% dfModel$b){
+        pred <- dfModel %>%
+            subset(a %in% word1 & b %in% word2) %>%
+            select(c) %>%
+            table %>% 
+            sort(decreasing = T) %>%
+            names %>%
+            head
+    }else if(word2 %in% dfModel$b){
+        pred <- dfModel %>%
+            subset(b %in% word2) %>%
+            select(c) %>%
+            table %>%
+            sort(decreasing = T) %>%
+            names %>%
+            head
+    }else{
+        pred <- dfModel %>%
+            select(c) %>%
+            table %>%
+            sort(decreasing = T) %>%
+            names %>%
+            head
+    }
+    pred
+}
 
