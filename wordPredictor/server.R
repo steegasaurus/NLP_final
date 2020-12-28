01d4e90ed95937615b3cfb82342ad7f9eb372f1b
Steeg Pierce <64050681+steegasaurus@users.noreply.github.com>
2020-12-27 22:41:48 -0600

    Update server.R

--- wordPredictor/server.R
+++ wordPredictor/server.R
@@ -1,17 +1,34 @@
 source('model.R')
 library(shiny)
+library(quanteda)
+library(tidyr)
+library(dplyr)
 
-# Define server logic required to draw a histogram
+# Define server logic
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$prediction <- renderDataTable({
+        
+        userText <- input$Text %>%
+            tokens(remove_punct = T,
+                   remove_symbols = T,
+                   remove_separators = T,
+                   remove_numbers = T,
+                   remove_url = T) %>%
+            tokens_ngrams(n = 2) %>%
+            unlist %>%
+            last %>%
+            strsplit('_') %>%
+            unlist
+        word1 <- userText[1]
+        word2 <- userText[2]
 
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        # x    <- faithful[, 2]
+        #bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        #hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
     })
 
